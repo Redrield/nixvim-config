247e32608b65ac6b4d59280278b21d32df9a1256
Kaitlyn Kenwell <redrield@gmail.com>
2024-06-07 14:11:02 -0400

    Bork

--- plugins/cmp.nix
+++ plugins/cmp.nix
@@ -7,6 +7,6 @@
     plugins.cmp-buffer.enable = true;
     plugins.cmp-nvim-lsp.enable = true;
     plugins.cmp-path.enable = true;
-    plugins.cmp-cmdline = true;
+    plugins.cmp-cmdline.enable = true;
   };
 }

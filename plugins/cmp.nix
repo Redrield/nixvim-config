0d4d6d089eb05fd29334360d6d2dc3aedf5744d3
Kaitlyn Kenwell <redrield@gmail.com>
2024-06-07 14:17:08 -0400

    Setting up cmp?

--- plugins/cmp.nix
+++ plugins/cmp.nix
@@ -2,11 +2,39 @@
 {
   opts = {
     enable = true;
+    sources = [
+      { name = "nvim_lsp"; }
+      { name = "path"; }
+      { name = "buffer"; }
+      { name = "cmdline"; }
+    ];
+
+    mapping = {
+      "<CR>" = "cmp.mapping.confirm({ select = true  })";
+      "<Tab>" = {
+        action = ''
+          function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end
+        '';
+      };
+    };
   };
   rootOpts = {
     plugins.cmp-buffer.enable = true;
     plugins.cmp-nvim-lsp.enable = true;
     plugins.cmp-path.enable = true;
     plugins.cmp-cmdline.enable = true;
+
+    keymaps = [
+      {
+        action = "<cmd>Telescope live_grep<CR>";
+        key = "<leader>g";
+      }
+    ];
   };
 }

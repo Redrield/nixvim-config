30b7c3a84a639f9bec22768b2b1c773ce0644c32
Kaitlyn Kenwell <redrield@gmail.com>
2024-06-07 10:04:16 -0400

    Pass config through start cmd instead

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -8,10 +8,11 @@
     };
     # Servers for golang and rust will be provided by dev envs, don't install packages.
     servers.rust-analyzer = {
+      installCargo = false;
       enable = true;
       autostart = true;
       package = null;
-      extraOptions = builtins.fromJSON (builtins.readFile "$HOME/.config/lsp/rust.json");
+      cmd = [ "rust-analyzer" "--config-path" "$HOME/.config/lsp/rust.json" ];
     };
     servers.gopls = {
       enable = true;

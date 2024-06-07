efee2219899a25a9df006a29320165911b140da6
Kaitlyn Kenwell <redrield@gmail.com>
2024-06-07 10:28:40 -0400

    Ignore config for now, fix later

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -6,13 +6,14 @@
       enable = true;
       autostart = true;
     };
+    #TODO: Dynamically put together the server configs from subfiles
     # Servers for golang and rust will be provided by dev envs, don't install packages.
     servers.rust-analyzer = {
       installCargo = false;
+      installRustc = false;
       enable = true;
       autostart = true;
       package = null;
-      cmd = [ "rust-analyzer" "--config-path" "$HOME/.config/lsp/rust.json" ];
     };
     servers.gopls = {
       enable = true;
